651ab871bce239728f2bc67d63db706c98f676ef
MohdArifFarooqui <paraspur.cscgonda@gmail.com>
2026-09-08 21:20:04 +0530

    Import math functions for distance calculations

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime
 from zoneinfo import ZoneInfo
+from math import radians, sin, cos, sqrt, atan2
 
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from werkzeug.security import generate_password_hash, check_password_hash
