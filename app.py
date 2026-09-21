e04ca7abd9a4c03fbfc5b1aad9e0c807eea8d344
MohdArifFarooqui <paraspur.cscgonda@gmail.com>
2026-09-22 02:36:22 +0530

    Fix indentation for distance_km assignment

--- app.py
+++ app.py
@@ -3712,7 +3712,7 @@ def worker_dashboard():
         else:
             request_distances[r.id] = None
 
-          r.distance_km = request_distances[r.id]
+            r.distance_km = request_distances[r.id]
      
     return render_template(
         "worker_dashboard.html",
